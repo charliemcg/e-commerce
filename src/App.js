7093f7bc373939c4e8a47f264df5bef5b8bac714
Charlie McGregor <charlie_mcgregor@outlook.com>
2021-06-20 19:49:37 +1000

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {ROUTES} from "./constants";
+import Products from "./components/products";
+import FourOhFour from "./components/404";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>Header</div>
+      <div>
+        <Switch>
+          <Route exact path={ROUTES.PRODUCTS} component={Products} />
+          <Route component={FourOhFour} />
+        </Switch>
+      </div>
+      <div>Footer</div>
+    </Router>
   );
 }
 
